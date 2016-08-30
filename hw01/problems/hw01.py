17b45c44fe2885333d4f234a661c21f2eab829b5
amipatel <abhinavrpatel@gmail.com>
2016-08-30 14:14:10 -0700

    init + added doctest comments

--- hw01/problems/hw01.py
+++ hw01/problems/hw01.py
@@ -2,6 +2,13 @@ from operator import add, sub
 
 
 def a_plus_abs_b(a, b):
+    """Return a+abs(b), but without calling abs.
+
+    >>> a_plus_abs_b(2, 3)
+    5
+    >>> a_plus_abs_b(2, -3)
+    5
+    """
     if b < 0:
         f = lambda x, y: sub(x, y)
     else:
@@ -12,6 +19,18 @@ def a_plus_abs_b(a, b):
 
 
 def two_of_three(a, b, c):
+    """Return x*x + y*y, where x and y are the two largest members of the
+    positive numbers a, b, and c.
+
+    >>> two_of_three(1, 2, 3)
+    13
+    >>> two_of_three(5, 3, 1)
+    34
+    >>> two_of_three(10, 2, 8)
+    164
+    >>> two_of_three(5, 5, 5)
+    50
+    """
     return sub(add(a*a, b*b, c*c), min(a,b,c))
 
 
@@ -20,6 +39,17 @@ def two_of_three(a, b, c):
 
 
 def largest_factor(n):
+    """Return the largest factor of n that is smaller than n.
+
+    >>> largest_factor(15) # factors are 1, 3, 5
+    5
+    >>> largest_factor(80) # factors are 1, 2, 4, 5, 8, 10, 16, 20, 40
+    40
+    >>> largest_factor(13) # factor is 1 since 13 is prime
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
     if n <= 1:
         return 1
 
@@ -44,16 +74,16 @@ def largest_factor(n):
 def if_function(condition, true_result, false_result):
     """Return true_result if condition is a true value, and
     false_result otherwise.
-    #>>> if_function(True, 2, 3)
+
+    >>> if_function(True, 2, 3)
     2
-    #>>> if_function(False, 2, 3)
+    >>> if_function(False, 2, 3)
     3
-    #>>> if_function(3==2, 3+2, 3-2)
+    >>> if_function(3==2, 3+2, 3-2)
     1
-    #>>> if_function(3>2, 3+2, 3-2)
+    >>> if_function(3>2, 3+2, 3-2)
     5
     """
-
     if condition:
         return true_result
     else:
