4ba80712f9a5e609db898d234e9d326a3b2c9f34
amipatel <abhinavrpatel@gmail.com>
2016-08-31 22:04:18 -0700

    init

--- hog/dice.py
+++ hog/dice.py
@@ -22,8 +22,17 @@ def make_fair_dice(sides):
         return randint(1,sides)
     return dice
 
+
+
+
+
+
 four_sided = make_fair_dice(4)
 six_sided = make_fair_dice(6)
+
+
+
+
 
 def make_test_dice(*outcomes):
     """Return a die that cycles deterministically through OUTCOMES.
