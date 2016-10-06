7fe250a513c22f774f2642696692665a2dee5633
amipatel <abhinavrpatel@gmail.com>
2016-10-06 16:01:37 -0700

    q2 done

--- ants/ants.py
+++ ants/ants.py
@@ -25,7 +25,8 @@ class Place(object):
         self.entrance = None  # A Place
         # Phase 1: Add an entrance to the exit
         # BEGIN Problem 2
-        "*** REPLACE THIS LINE ***"
+        if exit:
+            self.exit.entrance = self
         # END Problem 2
 
     def add_insect(self, insect):
