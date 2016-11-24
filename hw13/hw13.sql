6d1e90ebb0b208920aaf95e1a826705fbdff60a9
amipatel <abhinavrpatel@gmail.com>
2016-11-23 19:15:43 -0800

    done

--- hw13/hw13.sql
+++ hw13/hw13.sql
@@ -28,35 +28,63 @@ create table sizes as
 -------------------------------------------------------------
 
 -- The size of each dog
-create table size_of_dogs as
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE size_of_dogs AS
+  SELECT name, size FROM dogs, sizes WHERE height > min AND height <= max;
 
 -- All dogs with parents ordered by decreasing height of their parent
-create table by_height as
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE by_height AS
+  SELECT child FROM dogs, parents WHERE name = parent ORDER BY -height;
+
+
 
 -- Sentences about siblings that are the same size
-create table sentences as
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE sentences AS
+  WITH
+    siblings(first, second) AS (
+      SELECT a.child, b.child FROM parents AS a, parents AS b
+        WHERE a.parent = b.parent AND a.child < b.child
+    )
+  SELECT first || " and " || second || " are " || a.size || " siblings"
+    FROM siblings, size_of_dogs AS a, size_of_dogs AS b
+    WHERE a.size = b.size AND a.name = first AND b.name = second;
+
+
 
 -- Ways to stack 4 dogs to a height of at least 170, ordered by total height
-create table stacks as
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE stacks AS
+  WITH
+    sums(names, total, n, max) AS (
+      SELECT name, height, 1, height FROM dogs UNION
+      SELECT names || ", " || name, total+height, n+1, height FROM sums, dogs
+        WHERE n < 4 AND max < height
+    )
+  SELECT names, total FROM sums WHERE n=4 AND total>=170 ORDER BY total;
+
+
 
 -- non_parents is an optional, but recommended question
 -- All non-parent relations ordered by height difference
-create table non_parents as
-  select "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE non_parents AS
+  SELECT "REPLACE THIS LINE WITH YOUR SOLUTION";
 
-create table ints as
-    with i(n) as (
-        select 1 union
-        select n+1 from i limit 100
+CREATE TABLE ints AS
+    WITH i(n) AS (
+        SELECT 1 UNION
+        SELECT n+1 FROM i limit 100
     )
-    select n from i;
+    SELECT n FROM i;
 
-create table divisors as
-    select "REPLACE THIS LINE WITH YOUR SOLUTION";
 
-create table primes as
-    select "REPLACE THIS LINE WITH YOUR SOLUTION";
+CREATE TABLE divisors AS
+    WITH factors(i, j) AS (
+        SELECT i.n,
+        (SELECT count(j.n) FROM ints AS j WHERE i.n % j.n = 0 AND j.n <= 100)
+        FROM ints AS i
+    )
+    SELECT i, j FROM factors;
+
+
+
+
+CREATE TABLE primes AS
+  SELECT i FROM divisors WHERE j = 2 ORDER BY i ASC;
